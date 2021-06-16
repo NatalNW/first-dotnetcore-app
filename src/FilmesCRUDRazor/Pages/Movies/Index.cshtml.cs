78157ba033a5aadcffdba9e564e6be8576c87337
NatalNW <natanaelweslley1@gmail.com>
2021-06-15 23:12:54 -0300

    create form to filter by movie genre and search by tilte

--- src/FilmesCRUDRazor/Pages/Movies/Index.cshtml.cs
+++ src/FilmesCRUDRazor/Pages/Movies/Index.cshtml.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using FilmesCRUDRazor.Models;
@@ -20,9 +20,26 @@ namespace FilmesCRUDRazor.Pages.Movies
 
         public IList<Movie> Movie { get;set; }
 
-        public async Task OnGetAsync()
+        public SelectList Genres { get; set; }
+
+        public string FilteredByGenre { get; set; }
+
+        public async Task OnGetAsync(string searchByTitle, string filteredByGenre)
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> queryGenre = from m in _context.Movie orderby m.Genre select m.Genre;
+
+            var movies = from m in _context.Movie select m;
+
+            if(!String.IsNullOrEmpty(searchByTitle)){
+                movies = movies.Where(search => search.Title.Contains(searchByTitle));
+            }
+
+            if(!String.IsNullOrEmpty(filteredByGenre)){
+                movies = movies.Where(filtered => filtered.Genre == filteredByGenre);
+            }
+
+            Genres = new SelectList(await queryGenre.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
