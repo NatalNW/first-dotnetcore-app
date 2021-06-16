37b2a0af1dbe73ac7759618349f5723783758d4f
NatalNW <natanaelweslley1@gmail.com>
2021-06-15 21:48:50 -0300

    add require flag for some attributes

--- src/FilmesCRUDRazor/Models/Movie.cs
+++ src/FilmesCRUDRazor/Models/Movie.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmesCRUDRazor.Models
 {
@@ -6,8 +7,13 @@ namespace FilmesCRUDRazor.Models
     {
         public int MovieID { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required]
         public DateTime ReleaseDate { get; set; }
 
         public string Genre { get; set; }
